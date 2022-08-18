413f506307705518e02958fe885cbfca09685003
Egor_Potopeiko <79351048+EgorPotopeiko@users.noreply.github.com>
2022-08-18 17:51:42 +0300

    Start adaptive

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,5 +1,5 @@
-import React, {FC} from 'react';
-import {Container} from "@mui/material";
+import React, {FC, useState} from 'react';
+import {Collapse, Container, Divider, List, ListItemButton, ListItemText} from "@mui/material";
 import {Link} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {setPage} from "../../store/pagination/actions";
@@ -9,12 +9,31 @@ const menu_items = ['characters', 'comics', 'creators', 'events', 'series', 'sto
 
 const Menu: FC = () => {
     const dispatch = useDispatch();
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+        setOpen(!open);
+    };
     return (
         <div className="menu">
             <Container maxWidth='lg'>
                 {menu_items.map((elem) => (
                     <Link key={elem.toLowerCase()} className="menu__item" onClick={() => dispatch(setPage(1))} to={`/${elem}`}>{elem.toUpperCase()}</Link>
                 ))}
+                <div className="menu__adaptive">
+                    <ListItemButton onClick={handleClick}>
+                        <ListItemText primary="MENU" />
+                    </ListItemButton>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                {menu_items.map((elem) => (
+                                    <Link key={elem.toLowerCase()} className="menu__item-adaptive" onClick={() => dispatch(setPage(1))} to={`/${elem}`}>{elem.toUpperCase()}</Link>
+                                ))}
+                            </ListItemButton>
+                        </List>
+                    </Collapse>
+                </div>
+
             </Container>
         </div>
     );
--- src/pages/characters/page_characters/page_characters.tsx
+++ src/pages/characters/page_characters/page_characters.tsx
@@ -24,7 +24,7 @@ const PageCharacters: FC = () => {
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 16 }}>
                     {isLoading && (<Loader />)}
                     {!isLoading && (characters.map((character: TCharacter) => (
-                        <Grid item xs={2} sm={4} md={4} key={character.id}>
+                        <Grid item xs={2} sm={2.5} md={4} key={character.id}>
                             <CardPerson data-testid="person_test-id" person={character} />
                         </Grid>
                     )))}
